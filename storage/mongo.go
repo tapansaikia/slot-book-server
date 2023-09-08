e93ce74818af30523cd5da701510d2956193e056
tapan <tsaikia@fanatics.com>
2023-09-08 17:08:13 +0530

    connect to mongo db

--- storage/mongo.go
+++ storage/mongo.go
@@ -1,9 +1,47 @@
 package storage
 
-type MongoStorage struct{}
+import (
+	"context"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const uri = "mongodb+srv://tsaikia:wQzDAZFO5yQJdu85@cluster0.kmilyai.mongodb.net/?retryWrites=true&w=majority"
+
+type MongoStorage struct {
+	client *mongo.Client
+}
 
 func NewMongoStorage() *MongoStorage {
-	return &MongoStorage{}
+
+	// Use the SetServerAPIOptions() method to set the Stable API version to 1
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
+
+	// Create a new client and connect to the server
+	client, err := mongo.Connect(context.TODO(), opts)
+	if err != nil {
+		panic(err)
+	}
+	// defer func() {
+	// 	if err = client.Disconnect(context.TODO()); err != nil {
+	// 		panic(err)
+	// 	}
+	// }()
+
+	// Send a ping to confirm a successful connection
+	var result bson.M
+	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{Key: "ping", Value: 1}}).Decode(&result); err != nil {
+		panic(err)
+	}
+	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
+
+	return &MongoStorage{
+		client: client,
+	}
 }
 
 func (ms *MongoStorage) Get(id int) {
